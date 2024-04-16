b909165af2eab15e8823ac08bccf71b0144f31a6
Humbertin07 <130792797+Humbertin07@users.noreply.github.com>
2024-04-16 18:55:37 -0300

    Att
    O Código adicionado, recebe em tx, manda para um endereço, e imprime no lcd

--- PJ_FORCA.asm
+++ PJ_FORCA.asm
@@ -28,3 +28,225 @@ MAIN:
       RET
 
 END
+
+;Nesse trecho em diante, temos um código funcional...
+
+; PROGRAMA DO RECEPTOR: RECEBE VALORES PELO CANAL SERIAL E
+; ARMAZENA O VALOR NO ENDEREÇO 30H
+    RS      equ     P1.3    ;Reg Select ligado em P1.3
+    EN      equ     P1.2    ;Enable ligado em P1.2
+
+org 000h
+jmp start
+org 023H ; PONTEIRO DA INTERRUPCAO PARA CANAL SERIAL
+MOV A,SBUF ; REALIZA A LEITURA DO BYTE RECEBIDO
+setB F0
+MOV 30h, A ; ESCREVE O VALOR NO ENDEREÇO 30H
+CLR RI ; RESETA RI PARA RECEBER NOVO BYTE
+RETI
+start:
+ORG 200H
+; ***************************************************
+; PROGRAMA ESTÁ CONFIGURADO COM 4800 DE BAUD RATE
+; SEM PARIDADE E CT1 NO MODO 2 E SERIAL NO MODO 1
+; ***************************************************
+MOV SCON, #50H ;porta serial no modo 1 e habilita a recepção
+MOV PCON, #80h ;set o bit SMOD
+MOV TMOD, #20H ;CT1 no modo 2
+MOV TH1, #243 ;valor para a recarga
+MOV TL1, #243 ;valor para a primeira contagem
+MOV IE, #90H ; Habilita interrupção serial
+SETB TR1 ;liga o contador/temporizador 1
+acall lcd_init
+	mov A, #06h
+	ACALL posicionaCursor 
+	jnb F0, $
+	clr F0
+	MOV A, 30h
+	ACALL sendCharacter	
+JMP $
+
+lcd_init:
+
+	CLR RS		; clear RS - indicates that instructions are being sent to the module
+
+; function set	
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear	
+					; function set sent for first time - tells module to go into 4-bit mode
+; Why is function set high nibble sent twice? See 4-bit operation on pages 39 and 42 of HD44780.pdf.
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+					; same function set high nibble sent a second time
+
+	SETB P1.7		; low nibble set (only P1.7 needed to be changed)
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+				; function set low nibble sent
+	CALL delay		; wait for BF to clear
+
+
+; entry mode set
+; set to increment with no shift
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.6		; |
+	SETB P1.5		; |low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+
+
+; display on/off control
+; the display is turned on, the cursor is turned on and blinking is turned on
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.7		; |
+	SETB P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+sendCharacter:
+	SETB RS  		; setb RS - indicates that data is being sent to module
+	MOV C, ACC.7		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay			; wait for BF to clear
+	RET
+
+;Posiciona o cursor na linha e coluna desejada.
+;Escreva no Acumulador o valor de endere�o da linha e coluna.
+;|--------------------------------------------------------------------------------------|
+;|linha 1 | 00 | 01 | 02 | 03 | 04 |05 | 06 | 07 | 08 | 09 |0A | 0B | 0C | 0D | 0E | 0F |
+;|linha 2 | 40 | 41 | 42 | 43 | 44 |45 | 46 | 47 | 48 | 49 |4A | 4B | 4C | 4D | 4E | 4F |
+;|--------------------------------------------------------------------------------------|
+posicionaCursor:
+	CLR RS	         ; clear RS - indicates that instruction is being sent to module
+	SETB P1.7		    ; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay			; wait for BF to clear
+	RET
+
+
+;Retorna o cursor para primeira posi��o sem limpar o display
+retornaCursor:
+	CLR RS	      ; clear RS - indicates that instruction is being sent to module
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+;Limpa o display
+clearDisplay:
+	CLR RS	      ; clear RS - indicates that instruction is being sent to module
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+delay:
+	MOV R0, #50
+	DJNZ R0, $
+	RET
